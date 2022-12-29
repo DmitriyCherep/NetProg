34e4a942aeece1549b08de2ddd705eef1db8227e
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2022-12-29 14:49:40 +0300

    Update echo.cpp

--- echo/echo.cpp
+++ echo/echo.cpp
@@ -16,7 +16,7 @@ int main (int argc, char **argv)
 	srv_addr->sin_port = htons(7);
 	srv_addr->sin_addr.s_addr = inet_addr("172.16.40.1");
 	char *buf = new char[1024];
-	string str ("DANILA_TONUS\n");
+	string str ("MITYA_CHEREP\n");
 	int msg = str.length();
 	size_t length = str.copy(buf,msg);
 	int s = socket(AF_INET, SOCK_STREAM, 0);
